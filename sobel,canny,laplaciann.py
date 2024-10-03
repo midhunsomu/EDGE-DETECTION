ed3489f3f8f4cf8fd07731b360af37d0e5a08823
Midhun S <139331462+midhunsomu@users.noreply.github.com>
2024-10-03 15:30:15 +0530

    Update sobel,canny,laplaciann.py

--- sobel,canny,laplaciann.py
+++ sobel,canny,laplaciann.py
@@ -1,66 +1,71 @@
 import cv2
 import matplotlib.pyplot as plt
 
-//TYPE THE CODE HERE TO READ THE IMAGE UISNG IMREAD 
-gray=cv2.cvtColor(img,)//CONVERT THE COLOR TO GRAY TO RGB
-gray = cv2.GaussianBlur(gray,(3,3),0)
+img = cv2.imread("flower.jpeg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (3, 3), 0)
 
-sobelx = cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5)
+sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=5)
 plt.figure(figsize=(8,8))
-plt.subplot(1,2,1)
-plt.imshow(gray)
+plt.subplot(1, 2, 1)
+plt.imshow(gray, cmap='gray')
 plt.title("Original Image")
 plt.axis("off")
-plt.subplot(1,2,2)
-plt.imshow(sobelx)
+
+plt.subplot(1, 2, 2)
+plt.imshow(sobelx, cmap='gray')
 plt.title("Sobel X axis")
 plt.axis("off")
 plt.show()
 
-sobely = cv2.Sobel()//TYPE THE CODE HERE 
+sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=5)
 plt.figure(figsize=(8,8))
-plt.subplot(1,2,1)
-plt.imshow(gray)
+plt.subplot(1, 2, 1)
+plt.imshow(gray, cmap='gray')
 plt.title("Original Image")
 plt.axis("off")
-plt.subplot(1,2,2)
-plt.imshow(sobely)
+
+plt.subplot(1, 2, 2)
+plt.imshow(sobely, cmap='gray')
 plt.title("Sobel Y axis")
 plt.axis("off")
 plt.show()
 
-sobelxy = cv2.Sobel(gray,cv2.CV_64F,1,1,ksize=5)
+sobelxy = cv2.Sobel(gray, cv2.CV_64F, 1, 1, ksize=5)
 plt.figure(figsize=(8,8))
-plt.subplot(1,2,1)
-plt.imshow(gray)
+plt.subplot(1, 2, 1)
+plt.imshow(gray, cmap='gray')
 plt.title("Original Image")
 plt.axis("off")
-//TYPE THE CODE HERE
-plt.imshow(sobelxy)
+
+plt.subplot(1, 2, 2)
+plt.imshow(sobelxy, cmap='gray')
 plt.title("Sobel XY axis")
 plt.axis("off")
 plt.show()
 
-lap=cv2.Laplacian(gray,cv2.CV_64F)
+lap = cv2.Laplacian(gray, cv2.CV_64F)
 plt.figure(figsize=(8,8))
-plt.subplot(1,2,1)
-plt.imshow(gray)
+plt.subplot(1, 2, 1)
+plt.imshow(gray, cmap='gray')
 plt.title("Original Image")
 plt.axis("off")
-plt.subplot(1,2,2)
-//DISPLAY THE IMAGE USING IMSHOW 
+
+plt.subplot(1, 2, 2)
+plt.imshow(lap, cmap='gray')
 plt.title("Laplacian Edge Detector")
 plt.axis("off")
 plt.show()
 
-canny=cv2.Canny(gray,120,150)
+canny = cv2.Canny(gray, 120, 150)
 plt.figure(figsize=(8,8))
-plt.subplot(1,2,1)
-plt.imshow(gray)
+plt.subplot(1, 2, 1)
+plt.imshow(gray, cmap='gray')
 plt.title("Original Image")
 plt.axis("off")
-plt.subplot(1,2,2)
-plt.imshow(canny)
-//PROVIDE THE TITLE OF THE IMAGE DISPLAYED PAGE
+
+plt.subplot(1, 2, 2)
+plt.imshow(canny, cmap='gray')
+plt.title("Canny Edge Detector")
 plt.axis("off")
 plt.show()
